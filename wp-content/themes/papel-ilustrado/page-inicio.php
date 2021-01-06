4451237ee089f2549f5ea2ea04fa247773bcb8c6
oscar-soto <oscarantonio.soto1998@gmail.com>
2021-01-06 09:49:13 -0300

    Feat: hero section created

--- wp-content/themes/papel-ilustrado/page-inicio.php
+++ wp-content/themes/papel-ilustrado/page-inicio.php
@@ -6,6 +6,22 @@ get_header();
 
 ?>
 
-
+<!-- FIRST SECTION  -->
+<section class="container-fluid hero">
+   <div class="container">
+      <div class="hero__container">
+         <div class="hero__principal"  style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/png/hero1.png');">
+            <div class="hero__principal--desc" >
+               <h2>Tenemos la composicion que estas buscando</h2>
+               <a href="#">Ver Composiciones</a>
+            </div>
+         </div>
+         <div class="hero__card">
+            
+         </div>
+      </div>
+   </div>
+</section>
+<!-- FIRST SECTION  -->
 
 <?php get_footer();?>
