3002e9bae072227595af369b567f16f69f7556b5
oscar-soto <oscarantonio.soto1998@gmail.com>
2021-01-06 21:54:41 -0300

    Feat: card hero

---
+++ wp-content/themes/papel-ilustrado/components/single/principal-btn.php
@@ -0,0 +1,1 @@
+<a class="btn-principal" href="#">Ver Composiciones</a>
--- wp-content/themes/papel-ilustrado/page-inicio.php
+++ wp-content/themes/papel-ilustrado/page-inicio.php
@@ -13,11 +13,19 @@ get_header();
          <div class="hero__principal"  style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/png/hero1.png');">
             <div class="hero__principal--desc" >
                <h2>Tenemos la composicion que estas buscando</h2>
-               <a href="#">Ver Composiciones</a>
+               <?php get_template_part('components/single/principal-btn');?>
             </div>
          </div>
-         <div class="hero__card">
-            
+         <div class="row hero__card">
+            <div class="col-4 hero__card--container" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/png/hero2.png');">
+               <?php get_template_part('components/single/principal-btn');?>
+            </div>
+            <div class="col-4 hero__card--container" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/png/hero3.png');">
+               <?php get_template_part('components/single/principal-btn');?>
+            </div>
+            <div class="col-4 hero__card--container" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/png/hero4.png');">
+               <?php get_template_part('components/single/principal-btn');?>
+            </div>
          </div>
       </div>
    </div>
