d8b819e4c4ec4f828f040aa586f10a8c14172803
oscar-soto <oscarantonio.soto1998@gmail.com>
2021-01-08 00:13:22 -0300

    Feat: big card section created

--- wp-content/themes/papel-ilustrado/js/app.js
+++ wp-content/themes/papel-ilustrado/js/app.js
@@ -51,3 +51,10 @@ $(".paiting__slider").slick({
     dots: true,
     arrows: true,
 });
+$(".composition__slider").slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: true,
+});
