6b314a46b22f671d7a1c1c046e204b8d255085d6
oscar-soto <oscarantonio.soto1998@gmail.com>
2021-01-28 23:21:21 -0300

    Feat: descoraciones page created

---
+++ wp-content/themes/papel-ilustrado/components/single/card-twiceOut.php
@@ -0,0 +1,5 @@
+<div class="card__container--twiceOut">
+   <img class="principal" src="<?php echo get_template_directory_uri(); ?>/img/png/decoracion1.png" alt="">
+   <img class="secundary" src="<?php echo get_template_directory_uri(); ?>/img/png/decoracion2.png" alt="">
+   <?php get_template_part('components/single/twice-btn');?>
+</div>
