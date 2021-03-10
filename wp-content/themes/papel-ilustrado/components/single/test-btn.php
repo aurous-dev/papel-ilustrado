00f9514ad664a2b40e44553df751c76f09a19c70
oscar-soto <oscarantonio.soto1998@gmail.com>
2021-03-10 15:49:00 -0300

    Fix: add btn

---
+++ wp-content/themes/papel-ilustrado/components/single/test-btn.php
@@ -0,0 +1,12 @@
+<div class="contenedor">
+   <select name="prueba" id="prueba">
+      <option value="">Esto es el default</option>
+      <option value="blanco-encajonado"> Blanco Encajonado</option>
+      <option value="blanco-simple"> Blanco Simple</option>
+   </select>
+   <select name="prueba2" id="prueba2">
+      <option value="">Esto es el default</option>
+      <option value="30x30"> 30X30 </option>
+      <option value="30x40"> 30X40 </option>
+   </select>
+</div>
