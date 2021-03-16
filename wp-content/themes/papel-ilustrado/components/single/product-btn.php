3e6e6040498c41f77bcd2e69438ef317c3d1949d
Erasmo Hernandez <erasmoh@gmail.com>
2021-03-16 19:50:03 -0300

    Feat: Wishlist completed in backend, needs front

--- wp-content/themes/papel-ilustrado/components/single/product-btn.php
+++ wp-content/themes/papel-ilustrado/components/single/product-btn.php
@@ -2,23 +2,24 @@
    <div class="available">
       <div class="check">
          <svg width="11" height="11" viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M4.03113 7.27597L2.44071 5.68555C2.35508 5.59973 2.23883 5.5515 2.11759 5.5515C1.99635 5.5515 1.88009 5.59973 1.79446 5.68555C1.61571 5.8643 1.61571 6.15305 1.79446 6.3318L3.7103 8.24764C3.88905 8.42639 4.1778 8.42639 4.35655 8.24764L9.20571 3.39847C9.38446 3.21972 9.38446 2.93097 9.20571 2.75222C9.12008 2.6664 9.00383 2.61816 8.88259 2.61816C8.76135 2.61816 8.64509 2.6664 8.55946 2.75222L4.03113 7.27597Z" fill="white"/>
+            <path d="M4.03113 7.27597L2.44071 5.68555C2.35508 5.59973 2.23883 5.5515 2.11759 5.5515C1.99635 5.5515 1.88009 5.59973 1.79446 5.68555C1.61571 5.8643 1.61571 6.15305 1.79446 6.3318L3.7103 8.24764C3.88905 8.42639 4.1778 8.42639 4.35655 8.24764L9.20571 3.39847C9.38446 3.21972 9.38446 2.93097 9.20571 2.75222C9.12008 2.6664 9.00383 2.61816 8.88259 2.61816C8.76135 2.61816 8.64509 2.6664 8.55946 2.75222L4.03113 7.27597Z" fill="white" />
          </svg>
       </div>
       <div>Disponible</div>
    </div>
    <div class="heart-icon">
-      <a href="#">
+      <?php echo do_shortcode('[yith_wcwl_add_to_wishlist]'); ?>
+      <!-- <a href="#">
          <svg width="21" height="20" viewBox="0 0 22 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M12.3497 17.3072C11.5897 17.9972 10.4197 17.9972 9.65967 17.2972L9.54966 17.1972C4.29966 12.4472 0.869665 9.33723 0.999665 5.45723C1.05966 3.75723 1.92966 2.12723 3.33966 1.16723C5.97966 -0.632771 9.23967 0.207229 10.9997 2.26723C12.7597 0.207229 16.0197 -0.642771 18.6597 1.16723C20.0697 2.12723 20.9397 3.75723 20.9997 5.45723C21.1397 9.33723 17.6997 12.4472 12.4497 17.2172L12.3497 17.3072Z" fill="#F49A7E"/>
+            <path d="M12.3497 17.3072C11.5897 17.9972 10.4197 17.9972 9.65967 17.2972L9.54966 17.1972C4.29966 12.4472 0.869665 9.33723 0.999665 5.45723C1.05966 3.75723 1.92966 2.12723 3.33966 1.16723C5.97966 -0.632771 9.23967 0.207229 10.9997 2.26723C12.7597 0.207229 16.0197 -0.642771 18.6597 1.16723C20.0697 2.12723 20.9397 3.75723 20.9997 5.45723C21.1397 9.33723 17.6997 12.4472 12.4497 17.2172L12.3497 17.3072Z" fill="#F49A7E" />
          </svg>
          Guardar
-      </a>
+      </a> -->
    </div>
    <div class="size-icon">
       <a href="#" data-toggle="modal" data-target="#sizeModal">
          <svg width="22" height="18" viewBox="0 0 22 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M14 0H12V2H14V0ZM22 8H20V10H22V8ZM22 12H20V14H22V12ZM22 16H20V18C21 18 22 17 22 16ZM20 4H22V6H20V4ZM20 2V0C21 0 22 1 22 2H20ZM0 4H2V6H0V4ZM18 0H16V2H18V0ZM16 16H18V18H16V16ZM0 2C0 1 1 0 2 0V2H0ZM8 0H10V2H8V0ZM6 0H4V2H6V0ZM0 8V16C0 17.1 0.9 18 2 18H14V10C14 8.9 13.1 8 12 8H0ZM4.12 13.28L2.63 15.19C2.37 15.52 2.61 16 3.02 16.01H11C11.41 16.01 11.65 15.54 11.4 15.21L9.18 12.25C8.99 11.98 8.59 11.98 8.39 12.24L6.29 14.94L4.9 13.27C4.69 13.02 4.32 13.03 4.12 13.28Z" fill="#2A3D42"/>
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M14 0H12V2H14V0ZM22 8H20V10H22V8ZM22 12H20V14H22V12ZM22 16H20V18C21 18 22 17 22 16ZM20 4H22V6H20V4ZM20 2V0C21 0 22 1 22 2H20ZM0 4H2V6H0V4ZM18 0H16V2H18V0ZM16 16H18V18H16V16ZM0 2C0 1 1 0 2 0V2H0ZM8 0H10V2H8V0ZM6 0H4V2H6V0ZM0 8V16C0 17.1 0.9 18 2 18H14V10C14 8.9 13.1 8 12 8H0ZM4.12 13.28L2.63 15.19C2.37 15.52 2.61 16 3.02 16.01H11C11.41 16.01 11.65 15.54 11.4 15.21L9.18 12.25C8.99 11.98 8.59 11.98 8.39 12.24L6.29 14.94L4.9 13.27C4.69 13.02 4.32 13.03 4.12 13.28Z" fill="#2A3D42" />
          </svg>
          Guía de tamaños
       </a>
@@ -48,7 +49,7 @@
    <div class="marco-icon">
       <a href="#" data-toggle="modal" data-target="#marcoModal">
          <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M10.0001 1.66699C5.40008 1.66699 1.66675 5.40033 1.66675 10.0003C1.66675 14.6003 5.40008 18.3337 10.0001 18.3337C14.6001 18.3337 18.3334 14.6003 18.3334 10.0003C18.3334 5.40033 14.6001 1.66699 10.0001 1.66699ZM10.0001 14.167C9.54175 14.167 9.16675 13.792 9.16675 13.3337V10.0003C9.16675 9.54199 9.54175 9.16699 10.0001 9.16699C10.4584 9.16699 10.8334 9.54199 10.8334 10.0003V13.3337C10.8334 13.792 10.4584 14.167 10.0001 14.167ZM9.16675 7.50033H10.8334V5.83366H9.16675V7.50033Z" fill="#355957"/>
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M10.0001 1.66699C5.40008 1.66699 1.66675 5.40033 1.66675 10.0003C1.66675 14.6003 5.40008 18.3337 10.0001 18.3337C14.6001 18.3337 18.3334 14.6003 18.3334 10.0003C18.3334 5.40033 14.6001 1.66699 10.0001 1.66699ZM10.0001 14.167C9.54175 14.167 9.16675 13.792 9.16675 13.3337V10.0003C9.16675 9.54199 9.54175 9.16699 10.0001 9.16699C10.4584 9.16699 10.8334 9.54199 10.8334 10.0003V13.3337C10.8334 13.792 10.4584 14.167 10.0001 14.167ZM9.16675 7.50033H10.8334V5.83366H9.16675V7.50033Z" fill="#355957" />
          </svg>
          Probar Marco
       </a>
@@ -92,68 +93,61 @@
                      </div>
 
                      <!-- Marco tester -->
-                        <?php if (get_field('mostrar_marco_tester') == 'show') : ?>
-                           <div id="sigle-product-vue">
-                              <div v-if="loading">
-                                 <div style="text-align:center;margin:0 auto;">Cargando...</div>
-                              </div>
-                              <div v-else>
-                                 <div class="row simple-product__modal" :style="cssVars">
-                                    <div class="col-md-5 col-12 simple-product__modal--img">
-                                       <div v-if="obra.images" class="box__image">
-                                          <div class="frame-picture" :style="cssVars" :class="Object.keys(selectedMarco).length === 0 ? 'none' : 'withFrame'">
-                                             <img :src="obra.images[0].src" alt="">
-                                             <div class="inner"></div>
-                                          </div>
+                     <?php if (get_field('mostrar_marco_tester') == 'show') : ?>
+                        <div id="sigle-product-vue">
+                           <div v-if="loading">
+                              <div style="text-align:center;margin:0 auto;">Cargando...</div>
+                           </div>
+                           <div v-else>
+                              <div class="row simple-product__modal" :style="cssVars">
+                                 <div class="col-md-5 col-12 simple-product__modal--img">
+                                    <div v-if="obra.images" class="box__image">
+                                       <div class="frame-picture" :style="cssVars" :class="Object.keys(selectedMarco).length === 0 ? 'none' : 'withFrame'">
+                                          <img :src="obra.images[0].src" alt="">
+                                          <div class="inner"></div>
                                        </div>
                                     </div>
-                                    <div class="col-md-7 col-12 simple-product__modal--info">
-                                       <div class="obra-container__title">
-                                          <h3 class="simple-product__title">Test de marcos</h3>
-                                       </div>
-                                       <div class="obra-container__price__info__cost">
-                                          <h5 
-                                          v-if="!selectedMarco.nombre_de_marco" 
-                                          class="simple-product__desc">Selecciona un marco clickeando en algún ícono de abajo.</h5>
-                                          <h5 
-                                             v-else
-                                             class="simple-product__desc"
-                                             >Tu marco seleccionado es: {{selectedMarco?.nombre_de_marco}}</h5>
-                                       </div>
-                                       <div 
-                                          v-if="selectedMarco.descripcion" 
-                                          class="simple-product__desc">
-                                          Descripción: {{selectedMarco?.descripcion}}
-                                       </div>
-                                       <div class="simple-product__size--form">
-                                          <div class="frame form__size">
-                                             <div class="form__title">Selecciona un marco</div>
-                                             <div class="frame-option">
-                                                <div v-for="option in obra.marcos" :key="option.id">
-                                                   <button @click="changeFrame(option, $event)">
-                                                      <img :src="option.icono" />
-                                                   </button>
-                                                </div>
-                                                <div>
-                                                   <button @click="changeFrame({}, $event)">
-                                                      <!-- Sin Marco -->
-                                                      <img src="<?php echo get_template_directory_uri(); ?>/img/png/no_marco.png" alt="">
-                                                   </button>
-                                                </div>
+                                 </div>
+                                 <div class="col-md-7 col-12 simple-product__modal--info">
+                                    <div class="obra-container__title">
+                                       <h3 class="simple-product__title">Test de marcos</h3>
+                                    </div>
+                                    <div class="obra-container__price__info__cost">
+                                       <h5 v-if="!selectedMarco.nombre_de_marco" class="simple-product__desc">Selecciona un marco clickeando en algún ícono de abajo.</h5>
+                                       <h5 v-else class="simple-product__desc">Tu marco seleccionado es: {{selectedMarco?.nombre_de_marco}}</h5>
+                                    </div>
+                                    <div v-if="selectedMarco.descripcion" class="simple-product__desc">
+                                       Descripción: {{selectedMarco?.descripcion}}
+                                    </div>
+                                    <div class="simple-product__size--form">
+                                       <div class="frame form__size">
+                                          <div class="form__title">Selecciona un marco</div>
+                                          <div class="frame-option">
+                                             <div v-for="option in obra.marcos" :key="option.id">
+                                                <button @click="changeFrame(option, $event)">
+                                                   <img :src="option.icono" />
+                                                </button>
+                                             </div>
+                                             <div>
+                                                <button @click="changeFrame({}, $event)">
+                                                   <!-- Sin Marco -->
+                                                   <img src="<?php echo get_template_directory_uri(); ?>/img/png/no_marco.png" alt="">
+                                                </button>
                                              </div>
                                           </div>
                                        </div>
-                                       <div class="obra-container__price__info__des">
-                                          Ahora que probaste el marco que deseas seleccionalo antes de agregar al carrito.
-                                       </div>
-                                       <div class="obra-container__price__info__des">
-                                          <a href="#" style="text-decoration:underline;">Ya probé el marco que quiero, ir a comprar</a>
-                                       </div>
+                                    </div>
+                                    <div class="obra-container__price__info__des">
+                                       Ahora que probaste el marco que deseas seleccionalo antes de agregar al carrito.
+                                    </div>
+                                    <div class="obra-container__price__info__des">
+                                       <a href="#" style="text-decoration:underline;">Ya probé el marco que quiero, ir a comprar</a>
                                     </div>
                                  </div>
                               </div>
                            </div>
-                        <?php endif; ?>
+                        </div>
+                     <?php endif; ?>
                      <!-- Marco tester -->
                   </div>
                </div>
