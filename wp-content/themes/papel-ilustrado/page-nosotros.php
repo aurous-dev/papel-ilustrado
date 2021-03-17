f92a43a0e0c60b8b9af309f85d5816858a2e1dd3
oscar-soto <oscarantonio.soto1998@gmail.com>
2021-03-16 21:02:20 -0300

    Fix: nosotros page created

---
+++ wp-content/themes/papel-ilustrado/page-nosotros.php
@@ -0,0 +1,48 @@
+<?php 
+
+/* Template Name: Nosotros */
+
+get_header();
+
+?>
+<div class="container-fluid about">
+   <?php get_template_part('components/single/title');?>
+   <div class="container">
+      <div class="about__hero">
+         <div class="about__hero--img">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/png/hero1.png" alt="">
+         </div>
+         <div class="about__hero--desc">
+            Puedes modifcar el tamaño y marco de cada cuadro o eliminar cuadros. Recuerda que esta personalización cambiará el look de cada cuadro y puede no ser similar al diseño original de la serie.
+         </div>
+      </div>
+      <div class="row about__container">
+         <div class="col-md-6 about__container--img">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/png/nosotros.png" alt="">
+         </div>
+         <div class="col-md-6 about__container--info">
+            <h3>¿A Dónde deseas enviar?</h3>
+            <div>
+               Puedes modifcar el tamaño y marco de cada cuadro o eliminar cuadros. Recuerda que esta personalización cambiará el look de cada cuadro y puede no ser similar al diseño original de la serie. Puedes modifcar el tamaño y marco de cada cuadro o eliminar cuadros. Recuerda que esta personalización cambiará el look de cada cuadro y puede no ser similar al diseño original de la serie.
+               <br><br>
+               Puedes modifcar el tamaño y marco de cada cuadro o eliminar cuadros. Recuerda que esta personalización cambiará el look de cada cuadro y puede no ser similar al diseño original de la serie.
+            </div>
+         </div>
+      </div>
+      <div class="row about__container">
+         <div class="col-md-6 about__container--img">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/png/nosotros.png" alt="">
+         </div>
+         <div class="col-md-6 about__container--info">
+            <h3>¿A Dónde deseas enviar?</h3>
+            <div>
+               Puedes modifcar el tamaño y marco de cada cuadro o eliminar cuadros. Recuerda que esta personalización cambiará el look de cada cuadro y puede no ser similar al diseño original de la serie. Puedes modifcar el tamaño y marco de cada cuadro o eliminar cuadros. Recuerda que esta personalización cambiará el look de cada cuadro y puede no ser similar al diseño original de la serie.
+               <br><br>
+               Puedes modifcar el tamaño y marco de cada cuadro o eliminar cuadros. Recuerda que esta personalización cambiará el look de cada cuadro y puede no ser similar al diseño original de la serie.
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+
+<?php get_footer();?>
