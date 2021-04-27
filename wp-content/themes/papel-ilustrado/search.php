6a1f42d1056b5f2e91bb4b88ffda9370e85a637c
Erasmo Hernández <erasmoh@gmail.com>
2021-04-27 18:56:44 -0400

    Fix: Replace search title items

--- wp-content/themes/papel-ilustrado/search.php
+++ wp-content/themes/papel-ilustrado/search.php
@@ -53,7 +53,7 @@ get_template_part('components/single/search-title'); ?>
 <section class="section__news">
     <div class="container">
         <div class="section__news--title">
-            <h2>Novedades</h2>
+            <h2>Series</h2>
         </div>
         <div class="fourColumn__slider">
             <?php get_template_part('components/group/card-paiting'); ?>
@@ -63,7 +63,7 @@ get_template_part('components/single/search-title'); ?>
 <section class="section__news">
     <div class="container">
         <div class="section__news--title">
-            <h2>Novedades</h2>
+            <h2>Novedades individuales</h2>
         </div>
         <div class="fiveColumn__slider">
             <?php get_template_part('components/group/card-picture'); ?>
