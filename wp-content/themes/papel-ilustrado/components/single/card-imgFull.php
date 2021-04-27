240323e8fa97104b6fb9e5f30a5bbd4517eb26a7
Erasmo Hernández <erasmoh@gmail.com>
2021-04-27 15:49:45 -0400

    Replace image content on home for compositions

--- wp-content/themes/papel-ilustrado/components/single/card-imgFull.php
+++ wp-content/themes/papel-ilustrado/components/single/card-imgFull.php
@@ -1,4 +1,10 @@
 <div class="card__container--img-full">
-   <img src="<?php echo get_template_directory_uri(); ?>/img/png/picture4.png" alt="">
-   <?php get_template_part('components/single/card-btn');?>
+   <?php
+   $image = wp_get_attachment_image_src(get_post_thumbnail_id($loop->post->ID), 'single-post-thumbnail');
+   if ($image) : ?>
+      <img src="<?php echo $image[0]; ?>" data-id="<?php echo $loop->post->ID; ?>">
+   <?php else : ?>
+      <img src="<?php echo get_template_directory_uri(); ?>/img/png/picture4.png" alt="">
+   <?php endif; ?>
+   <?php get_template_part('components/single/card-btn'); ?>
 </div>
--- wp-content/themes/papel-ilustrado/page-inicio.php
+++ wp-content/themes/papel-ilustrado/page-inicio.php
@@ -48,7 +48,7 @@ get_header();
 </section>
 <!-- SECOND SECTION  -->
 <!-- THIRD SECTION  -->
-<section class="container-fluid composition">
+<!-- <section class="container-fluid composition">
    <?php if (have_rows('titulo_de_seccion_composiciones')) : ?>
       <?php while (have_rows('titulo_de_seccion_composiciones')) : the_row(); ?>
          <?php get_template_part('components/single/title'); ?>
@@ -72,6 +72,44 @@ get_header();
          <?php endif; ?>
       </div>
    </div>
+</section> -->
+<section class="container-fluid composition">
+	<?php
+	$title = 'Composiciones';
+	set_query_var('title', $title);
+	get_template_part('components/single/title'); ?>
+	<div class="container">
+		<div class="composition__slider">
+			<?php
+			$value = get_query_var('value');
+
+			$args = array(
+				'post_type' => 'product',
+				'posts_per_page' => 10,
+				'meta_key'      => 'tipo_de_producto',
+				'meta_value'   => 'composicion'
+			);
+			$loop = new WP_Query($args);
+
+			while ($loop->have_posts()) : $loop->the_post(); ?>
+				<a href="<?php the_permalink(); ?>" class="big-card" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="<?php echo $rowNumber; ?>00">
+					<div>
+						<div class="big-card__container">
+							<div class="big-card__container--img" style="background-image: url('<?php global $product; echo wp_get_attachment_url( $product->get_image_id() ); ?>');"></div>
+							<?php
+							$title = get_the_title();
+							$button = 'Ver más';
+							set_query_var('title', $title);
+							set_query_var('button', $button);
+							get_template_part('components/single/card-special'); ?>
+						</div>
+					</div>
+				</a>
+			<?php endwhile;
+			wp_reset_query(); // Remember to reset 
+			?>
+		</div>
+	</div>
 </section>
 <!-- THIRD SECTION  -->
 <!-- FOURTH SECTION  -->
